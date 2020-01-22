9a401edf7d7aa9634ce00921c180ac56ededdbac
Salahudin Zumberi <zumbesa1@students.zhaw.ch>
2020-01-22 21:53:18 +0100

    My first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule } from '@angular/material';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { MapsComponent } from './maps/maps.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    MapsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
